16309877e5d109f0e700808c13eba9a85c58f9ea
gaoyang <2313332818@qq.com>
2019-12-18 21:58:29 +0800

    commit liquidator

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './reset-css.scss';
+import './App.scss';
+
+import Home from './home/home';
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <Home />
+    </div>
+  );
+}
+
+export default App;
